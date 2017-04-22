81dff846654ceed9e58106e0da1e86cc89c603fe
Your Name <you@example.com>
2017-04-22 15:40:53 +0300

    added Tests

---
+++ Blog.Ui.Tests/UI_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blog.Ui.Tests
+{
+    [TestClass]
+    public class UI_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
